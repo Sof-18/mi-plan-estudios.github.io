b6e3142cd348bd8ddd5cbb4ebf118f12df57225b
Sof-18 <sofia524capa@gmail.com>
2025-08-03 19:30:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function(){
                     subjects: [
                         { id: "analisis2", name: "Análisis Matemático II", credits: 5, requires: ["fisica1"] },
                         { id: "circuitos", name: "Introducción a los Circuitos Eléctricos", credits: 4, requires: ["quimica", "alg"] },
-                        { id: "probabilidad", name: "Probabilidad y Estadística", credits: 4, requires: [] },
+                        { id: "probabilidad", name: "Probabilidad y Estadística", credits: 4, requires: ["alg", "analisis1"] },
                         { id: "resistencia", name: "Estabilidad y Resistencia de Materiales", credits: 5, requires: ["sistemas-rep", "fisica1", "informatica"] }
                     ]
                 },
